12ca3c892817421e4414c38944e507e9683d19b0
ELYSIAN\chime <scott@bakkergames.com>
2018-01-20 11:58:26 -0500

    Added match -10, +10 buttons

--- app/src/main/java/bert133/bertscout2018/MatchActivity.java
+++ app/src/main/java/bert133/bertscout2018/MatchActivity.java
@@ -3,6 +3,8 @@ package bert133.bertscout2018;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MatchActivity extends AppCompatActivity {
@@ -16,6 +18,100 @@ public class MatchActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.TEAM_MESSAGE);
 
+        // set title
         this.setTitle(String.format("Match Scouting - Team %s", message));
+
+        // --- buttons ---
+
+        Button matchMinusButton = (Button) findViewById(R.id.stand_match_minus_btn);
+        matchMinusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView matchText = (TextView) findViewById(R.id.stand_match_number);
+                matchText.requestFocus();
+                int tempValue;
+                try {
+                    tempValue = Integer.parseInt(matchText.getText().toString());
+                    if (tempValue <= 1) {
+                        tempValue = 1;
+                    } else {
+                        tempValue--;
+                    }
+                } catch (Exception e) {
+                    tempValue = 1;
+                }
+                matchText.setText(Integer.toString(tempValue));
+            }
+        });
+
+        Button matchPlusButton = (Button) findViewById(R.id.stand_match_plus_btn);
+        matchPlusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView matchText = (TextView) findViewById(R.id.stand_match_number);
+                matchText.requestFocus();
+                int tempValue;
+                try {
+                    tempValue = Integer.parseInt(matchText.getText().toString());
+                    if (tempValue >= 999) {
+                        tempValue = 999;
+                    } else {
+                        tempValue++;
+                    }
+                } catch (Exception e) {
+                    tempValue = 1;
+                }
+                matchText.setText(Integer.toString(tempValue));
+            }
+        });
+
+        Button matchMinus10Button = (Button) findViewById(R.id.stand_match_minus10_btn);
+        matchMinus10Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView matchText = (TextView) findViewById(R.id.stand_match_number);
+                matchText.requestFocus();
+                int tempValue;
+                try {
+                    tempValue = Integer.parseInt(matchText.getText().toString());
+                    if (tempValue <= 10) {
+                        tempValue = 1;
+                    } else if (tempValue % 10 != 0) {
+                        tempValue = tempValue - (tempValue % 10);
+                    } else {
+                        tempValue=tempValue-10;
+                    }
+                } catch (Exception e) {
+                    tempValue = 1;
+                }
+                matchText.setText(Integer.toString(tempValue));
+            }
+        });
+
+        Button matchPlus10Button = (Button) findViewById(R.id.stand_match_plus10_btn);
+        matchPlus10Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView matchText = (TextView) findViewById(R.id.stand_match_number);
+                matchText.requestFocus();
+                int tempValue;
+                try {
+                    tempValue = Integer.parseInt(matchText.getText().toString());
+                    if (tempValue < 10) {
+                        tempValue = 10;
+                    } else if (tempValue >= 990 ) {
+                        tempValue = 999;
+                    } else if (tempValue % 10 != 0) {
+                        tempValue = tempValue + 10 - (tempValue % 10);
+                    } else {
+                        tempValue=tempValue+10;
+                    }
+                } catch (Exception e) {
+                    tempValue = 1;
+                }
+                matchText.setText(Integer.toString(tempValue));
+            }
+        });
+
     }
 }
