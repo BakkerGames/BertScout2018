06514e63218a85c5bbe2d4e094d6ff2e2554871b
ELYSIAN\chime <scott@bakkergames.com>
2018-01-21 15:07:31 -0500

    Adding team numbers, show in list

--- app/src/main/java/bert133/bertscout2018/AddTeamActivity.java
+++ app/src/main/java/bert133/bertscout2018/AddTeamActivity.java
@@ -2,6 +2,11 @@ package bert133.bertscout2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddTeamActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class AddTeamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_team);
+
+        Button addTeamButton = (Button) findViewById(R.id.add_teams_add_button);
+        addTeamButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText teamNumberEdit = (EditText) findViewById(R.id.add_teams_team_number_edit);
+                TextView teamNumberList = (TextView) findViewById(R.id.add_teams_list_text);
+                String value = String.valueOf(teamNumberEdit.getText());
+                while (value.startsWith("0")) {
+                    value = value.substring(1);
+                }
+                if (!value.equals("")) {
+                    if (String.valueOf(teamNumberList.getText()).length() > 0) {
+                        teamNumberList.append(", ");
+                    }
+                    teamNumberList.append(value);
+                    teamNumberEdit.setText("");
+                }
+            }
+        });
     }
 }
