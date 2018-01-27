c245396d44c926e5f597c8680e030dc02c016af0
Isaac Nutter <diablolol@outlook.com>
2018-01-27 14:40:19 -0500

    added buttons with toast

--- app/src/main/java/bert133/bertscout2018/SyncDataActivity.java
+++ app/src/main/java/bert133/bertscout2018/SyncDataActivity.java
@@ -2,6 +2,10 @@ package bert133.bertscout2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SyncDataActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class SyncDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync_data);
+
+        Button syncSendButton = (Button) findViewById(R.id.sync_data_send_button);
+        syncSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"sending",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button syncListenButton = (Button) findViewById(R.id.sync_data_listen_button);
+        syncListenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"listening",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
