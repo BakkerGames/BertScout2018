cb91c825fd208db78c30607dc9b53dd6b3a5d8dc
StarBurstLink <starburstlink@gmail.com>
2018-01-29 19:12:12 -0500

    Added comment classes

---
+++ app/src/main/java/bert133/bertscout2018/MatchComments.java
@@ -0,0 +1,12 @@
+package bert133.bertscout2018;
+
+/**
+ * Created by Jacob on 1/29/18.
+ */
+
+public class MatchComments {
+    public int teamNumber;
+    public int matchNumber;
+    public String idCode;
+    public String Comment;
+}
---
+++ app/src/main/java/bert133/bertscout2018/TeamComments.java
@@ -0,0 +1,11 @@
+package bert133.bertscout2018;
+
+/**
+ * Created by Jacob on 1/29/18.
+ */
+
+public class TeamComments {
+    public int teamNumber;
+    public String idCode;
+    public String Comment;
+}
